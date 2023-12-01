38ce6e74e8040965605efbb5695bbd68531d999c
K2H-X1\k2hyu <k2hyun4@naver.com>
2023-12-01 12:32:03 +0900

    init

---
+++ src/components/MainView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import axios from "axios";
+
+const check = async () => {
+  const {data} = await axios.get('/check');
+
+  alert(data);
+};
+</script>
+
+<template>
+  <div>
+    <p>Hello</p>
+    <button @click="check">Check</button>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang='ts'>
+import MainView from '@/components/MainView.vue';
+</script>
+
+<template>
+  <main>
+    <p>Hello World!</p>
+    <MainView/>
+  </main>
+</template>
